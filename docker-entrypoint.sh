efcbc221fb4262c3a16a6624aa1f515d38280fea
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2022-09-26 05:46:26 -0400

    copy key files from volume

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,4 +21,11 @@ SSH_PORT_LISTEN_DAEMON=${SSH_PORT_LISTEN_DAEMON:-2200}
 SSH_PORT_LISTEN_CLIENT=${SSH_PORT_LISTEN_CLIENT:-2201}
 SSH_PORT_ADVERTIZE=${SSH_PORT_ADVERTIZE:-${SSH_PORT_LISTEN_CLIENT}}
 
+set -xe
+mkdir -p /etc/tmate-ssh-server/keys
+# we assume that the key file have been mounted in /volume with these
+# specific names
+cp /volume/tmate-server-rsa-key /etc/tmate-ssh-server/keys/ssh_host_rsa_key
+cp /volume/tmate-server-ed25519-key /etc/tmate-ssh-server/keys/ssh_host_ed25519_key
+
 exec tmate-ssh-server -p "${SSH_PORT_LISTEN_DAEMON}" -c "${SSH_PORT_LISTEN_CLIENT}" -q "${SSH_PORT_ADVERTIZE}" -k "${SSH_KEYS_PATH}" "$@"
